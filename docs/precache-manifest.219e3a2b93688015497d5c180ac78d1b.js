10520a0698c7a2b6968a226b3cd740de11654c1b
Miguel Cardenas <miguel.cardenas@akzio.cl>
2020-08-04 11:34:14 -0400

    create docs

---
+++ docs/precache-manifest.219e3a2b93688015497d5c180ac78d1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56682cb831921dcea5128cbdbf4ca94b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2f2d369e14770f85cc0",
+    "url": "/static/css/main.df2a561d.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2f2d369e14770f85cc0",
+    "url": "/static/js/main.ef5e4b02.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
